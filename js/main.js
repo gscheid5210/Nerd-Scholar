2673d0dbd5d3e21818394076651a60d43abc34cd
romano-w <will.j.romano@gmail.com>
2020-02-23 03:04:41 -0500

    more attempts at establishing authentication, complications

--- js/main.js
+++ js/main.js
@@ -1,38 +1,47 @@
-function f1() {
-    alert("f1 called");
-    //form validation that recalls the page showing with supplied inputs.
-}
-window.onload = function() {
-    document.getElementById("register-account").onclick = function fun() {
-        createAccount();
-        //validation code to see State field is mandatory.
-    }
-}
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://nerdscholar-58c0a.firebaseio.com"
+});
+
+// window.onload = function() {
+//     document.getElementById("register-account").onclick = function fun() {
+//         createAccount();
+//         //validation code to see State field is mandatory.
+//     }
+// }
+//
+//
+//
+//
+// function createAccount(e) {
+//     // if (e.preventDefault) e.preventDefault();
+//     var firstname = document.getElementById("exampleFirstName").value;
+//     var lastname = document.getElementById("exampleLastName").value;
+//     var email = document.getElementById("exampleInputEmail").value;
+//     var password = document.getElementById("exampleInputPassword").value;
+//     var confirmpw = document.getElementById("exampleRepeatPassword").value;
+//     alert(firstname, lastname, email, password, confirmpw);
+//
+//     //Create User with Email and Password
+//     firebase.auth().createUserWithEmailAndPassword(mail, password).catch(function(error) {
+//       // Handle Errors here.
+//       var errorCode = error.code;
+//       var errorMessage = error.message;
+//       console.log(errorCode);
+//       console.log(errorMessage);
+//     });
+//
+//     // You must return false to prevent the default form behavior
+//     return false;
+// }
 
 
 
-
-function createAccount(e) {
-    // if (e.preventDefault) e.preventDefault();
-    var firstname = document.getElementById("exampleFirstName").value;
-    var lastname = document.getElementById("exampleLastName").value;
-    var email = document.getElementById("exampleInputEmail").value;
-    var password = document.getElementById("exampleInputPassword").value;
-    var confirmpw = document.getElementById("exampleRepeatPassword").value;
-    alert(firstname, lastname, email, password, confirmpw);
-
-    //Create User with Email and Password
-    firebase.auth().createUserWithEmailAndPassword(mail, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode);
-      console.log(errorMessage);
-    });
-
-    // You must return false to prevent the default form behavior
-    return false;
-}
 //
 // var form = document.getElementById('register-account');
 // if (form.attachEvent) {
@@ -40,7 +49,15 @@ function createAccount(e) {
 // } else {
 //     form.addEventListener("submit", createAccount);
 // }
-//
-// var email="someone@example.com";
-// var password="password";
-//
+
+var email="someone@example.com";
+var password="password";
+
+//Create User with Email and Password
+firebase.auth().createUserWithEmailAndPassword(mail, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log(errorCode);
+  console.log(errorMessage);
+});
